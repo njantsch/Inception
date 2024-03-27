ecbd6efda06dcaa7761953b9c7dc28226656eee9
Nicolas Jantsch <njantsch@1-A-4.42heilbronn.de>
2024-03-27 19:26:50 +0100

    substitute domain name as well in nginx config

--- srcs/requirements/nginx/tools/setup_nginx.sh
+++ srcs/requirements/nginx/tools/setup_nginx.sh
@@ -5,6 +5,6 @@ chmod 700 $CERTS_
 # creates self-signed certificate, in production a certificate from a CA is needed
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_/private.key -out $CERTS_/certificate.crt -subj "/C=DE/L=HN/O=42heilbronn/OU=student/CN=njantsch.42.fr"
 
-envsubst '${CERTS_}' < /default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${CERTS_} ${DOMAIN_NAME}' < /default.conf.template > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
